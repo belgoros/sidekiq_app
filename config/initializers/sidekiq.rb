3365914fbbf0de41f0141acfd2239e4033d26ec9
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-09-19 16:57:35 +0200

    Add REDIS_PROVIDER to credentials, update sidekiq.rb
    - rename 'JOB_WORKER_URL' to 'REDIS_PROVIDER' variable and save it to Rails credentials
    - add sidekiq server config to see when Sidekiq is started/stopped

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,21 @@
-sidekiq_config = { url: ENV['JOB_WORKER_URL'] }
+require 'sidekiq'
+
+sidekiq_config = { url: ENV['REDIS_PROVIDER'] }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
   config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+
+  # Touch a file so we know Sidekiq worker has started.
+  # Useful for health checks
+  config.on(:startup) do
+    FileUtils.touch(Rails.root.join("tmp", "pids", "sidekiq_started"))
+  end
+  # Delete the touched file so we know Sidekiq worker has stopped.
+  # Useful for health checks
+  config.on(:shutdown) do
+    FileUtils.rm(Rails.root.join("tmp", "pids", "sidekiq_started"))
+  end
 end
 
 Sidekiq.configure_client do |config|
